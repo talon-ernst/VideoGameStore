6be23e6bbeec00de86c66273276a5b1091a302c9
talon-ernst <talonernst@rogers.com>
2021-04-17 16:59:49 -0400

    Added Project

---
+++ VideoGameStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoGameStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
